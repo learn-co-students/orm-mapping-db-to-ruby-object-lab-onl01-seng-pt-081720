0824ce15cc5eede414654c1a138dfc4fbf9ab5e1
Christian Pardo <chrispardopersonal@gmail.com>
2020-10-27 00:17:13 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -23,6 +23,34 @@ class Student
         self.new_from_db(row)
       end 
   end
+  
+  def self.all_students_in_grade_9
+    sql = <<-SQL
+      SELECT * 
+      FROM students 
+      WHERE grade = 9 
+    SQL
+    
+    DB[:conn].execute(sql).map do |row|
+      self.new_from_db(row)
+    end 
+  end
+  
+  def self.students_below_12th_grade
+    sql = <<-SQL
+      SELECT * 
+      FROM students 
+      WHERE grade < 12
+    SQL
+    
+    DB[:conn].execute(sql).map do |row|
+      self.new_from_db(row)
+    end 
+  end 
+  
+  def self.first_X_students_in_grade_10
+    
+  end 
 
   def self.find_by_name(name)
     # find the student in the database given a name
